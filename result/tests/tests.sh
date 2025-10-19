0853560d6c3831656c2299713a698917b71e7da4
Favour Efe <enegbenojie@gmail.com>
2025-10-19 03:43:58 -0400

    test modified

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-while ! timeout 1 bash -c "echo > /dev/tcp/vote/80"; do
+# Wait for the 'vote' service to be reachable
+while ! nc -z vote 80 2>/dev/null; do
     sleep 1
 done
 
@@ -8,13 +9,13 @@ curl -sS -X POST --data "vote=b" http://vote > /dev/null
 sleep 10
 
 if phantomjs render.js http://result | grep -q '1 vote'; then
-  echo -e "\\e[42m------------"
-  echo -e "\\e[92mTests passed"
-  echo -e "\\e[42m------------"
+  echo -e "\e[42m------------"
+  echo -e "\e[92mTests passed"
+  echo -e "\e[42m------------"
   exit 0
 else
-  echo -e "\\e[41m------------"
-  echo -e "\\e[91mTests failed"
-  echo -e "\\e[41m------------"
+  echo -e "\e[41m------------"
+  echo -e "\e[91mTests failed"
+  echo -e "\e[41m------------"
   exit 1
 fi
